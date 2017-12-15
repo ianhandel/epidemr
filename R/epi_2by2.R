15e13cc6beddabbe7b2389e88d19d204c355e851
Ian Handel <ian.handel@ed.ac.uk>
2017-12-15 18:28:28 +0000

    add TODO asserthat tidyup code

--- R/epi_2by2.R
+++ R/epi_2by2.R
@@ -11,13 +11,46 @@
 #' @examples
 #' epi_2by2(mtcars, gear, carb)
 
+# TODO for output - inspired by EpiTools from Ausvet
+# 2 x 2 table!
+# expected values 2 x 2
+# Overall Incidence/Prevalence
+# Incidence/Prevalence in Exposed
+# Incidence/Prevalence in Unexposed
+# Odds Ratio
+# Relative Risk (Cross-sectional study)
+# Attributable Risk
+# Attributable fraction in Exposed
+# Population Attributable Risk
+# Population Attributable Fraction
+# Tests - statistic and p-value
+# Uncorrected Chi-square
+# Yates corrected Chi-square
+# Fisher's exact test (2-tailed)
+# Fisher's exact test (1-tailed)
+# McNemar's test (paired data)
+
+
+
 epi_2by2 <- function(df, outcome, exposure){
+
   outcome <- rlang::enquo(outcome)
   exposure <- rlang::enquo(exposure)
-  tbl <- table(df[[rlang::quo_text(outcome)]],
-               df[[rlang::quo_text(exposure)]])
+
+  df <- dplyr::mutate(df, ..outcome = !!outcome)
+  df <- dplyr::mutate(df, ..exposure = !!exposure)
+
+  assertthat::assert_that(length(unique(df[["..outcome"]])) == 2)
+  assertthat::assert_that(length(unique(df[["..exposure"]])) == 2)
+
   col_names <- c(rlang::quo_text(outcome), rlang::quo_text(exposure))
-  return(structure(list(table = tbl), class = "epi_2by2"))
+
+  tbl <- table(df[["..outcome"]],
+               df[["..exposure"]],
+               dnn = col_names)
+
+  return(structure(list(table = tbl),
+                   class = "epi_2by2"))
 }
 
 
