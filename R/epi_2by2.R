adc6ee3d2dd5848a3fbcbe91725c12f36354dc5e
Ian Handel <ian.handel@ed.ac.uk>
2017-12-15 08:24:19 +0000

    Skeleton for a 2by2 function to test non-standard evaluation

---
+++ R/epi_2by2.R
@@ -0,0 +1,22 @@
+
+
+#' Two by two table analysis
+#'
+#' A technology test rather than a final product
+#'
+#' Introduce S3 class epi and explore printing and plotting methods
+#'
+#' @param df A dataframe
+#' @param outcome The unquoted column name for outcome.
+#' @param exposure The unquoted column name for exposure
+#' @return To return relevant summary data.
+#' @examples
+#' epi_2by2(mtcars, gear, carb)
+
+epi_2by2 <- function(df, outcome, exposure){
+  outcome <- rlang::enquo(outcome)
+  exposure <- rlang::enquo(exposure)
+  table(df[[rlang::quo_text(outcome)]], df[[rlang::quo_text(exposure)]])
+}
+
+
