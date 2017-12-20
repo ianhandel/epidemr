8f9b1eacbffedeb9411a884f6a57ef9384c387c1
Ian Handel <ian.handel@ed.ac.uk>
2017-12-20 01:35:04 +0000

    add a very simple table test

---
+++ tests/testthat/test_epi_2by2.R
@@ -0,0 +1,11 @@
+# epi_2by2 tests
+
+library(epidemr)
+context("Simple table tests")
+
+
+
+test_that("function tabulates", {
+  expect_equivalent(epi_2by2(mtcars, outcome = am==1, exposure = cyl==4)$table,
+               table(mtcars$cyl ==4, mtcars$am == 1))
+})
