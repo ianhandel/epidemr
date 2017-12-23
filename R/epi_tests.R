88f445bdabbe58936238ec850e594145b8f7d7df
Ian Handel <ian.handel@ed.ac.uk>
2017-12-23 10:29:15 +0000

    add a twobytwo function and tweak epi_tests to allow direct table entry of data

--- R/epi_tests.R
+++ R/epi_tests.R
@@ -1,9 +1,9 @@
 #' Wrap data from a dataframe and send to epiR::epi.tests
 #'
-#' A wrapper for epirR::epi.tests  called with a data.frame and
-#' unquoted columns or expressions.
+#' A wrapper for epirR::epi.tests normally called with a data.frame and
+#' unquoted columns or expressions. Can also be called with an epi_twobytwo object (generated with twobytwo helper function) to allow direct entry of a table of results.
 #'
-#' @param df A dataframe
+#' @param x A dataframe or epi_two_by_two object
 #' @param test The unquoted column name for test results
 #' @param goldstandard The unquoted column name for gold standard test results
 #' @param conf_level Probability for confidence interval calculations
@@ -14,10 +14,18 @@
 #' epi_tests(mtcars, mpg < 25, cyl > 4, conf_level = 0.95)
 
 
-epi_tests <- function(df,
-                      test,
-                      goldstandard,
+epi_tests <- function(x,
+                      test = NULL,
+                      goldstandard = NULL,
                       conf_level = 0.95) {
+
+  if (class(x) == "epi_twobytwo"){
+    return(epiR::epi.tests(as.matrix(with(x, c(TP, FN, FP, TN)),
+                                     conf.level = conf_level)))
+  }
+
+  df <- x
+
   test <- rlang::enquo(test)
   goldstandard <- rlang::enquo(goldstandard)
 
@@ -42,4 +50,22 @@ epi_tests <- function(df,
   )
 
   epiR::epi.tests(tab, conf.level = conf_level)
+}
+
+#' Make an epi_twobytwo object for epi_tests
+#'
+#' A helper function for epi_tests that allows entry of summary data
+#'
+#' @param a The TRUE POSITIVE count
+#' @param b The FALSE POSITIVE count
+#' @param c The FALSE NEGATIVE count
+#' @param d The TRUE NEGATIVE count
+#' @return Returns an epi.tests
+#' @export
+#' @examples
+
+#' epi_tests(twobytwo(12, 14, 1, 5))
+
+twobytwo <- function(a, b, c, d){
+  structure(list(TP = a, FP = b, FN = c, TN = d), class = "epi_twobytwo")
 }
