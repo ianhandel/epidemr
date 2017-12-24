d5a24c36a969deea32956e34c61da5f0bd02a2ba
Ian Handel <ian.handel@ed.ac.uk>
2017-12-24 16:11:18 +0000

    basic epi_predval working

---
+++ R/epi_predval.R
@@ -0,0 +1,30 @@
+#' Generate ppv and npv from test evaluation and prevalence range
+#'
+
+#'
+#' @param x An epiR::epi.tests object or test sensitivity
+#' @param sp test specicifity if x is sensitivity
+#' @param prevalences vector of prevalences to calculate npv and ppv at
+#' @return Returns an epi_predval object
+#' @export
+#' @examples
+#' head(mtcars)
+#' res <- epi_tests(mtcars, mpg < 25, cyl > 4, conf.level = 0.95)
+#' epi_predval(res)
+
+epi_predval <-function(x = NULL, se = NULL, sp = NULL,
+                       prevalences = seq(0, 1, 0.05)){
+
+  if(class(x) == "epi.tests"){
+    se <- x$rval$se$est
+  }else{
+    if(is.null(se)) se  <- x
+  }
+  p <- prevalences
+
+  ppv <- (se * p) / ((se * p) + ((1 - sp) * (1 - p)))
+  npv <- sp * (1 - p) / ((sp * (1 - p) + (1 - se) * p))
+
+  structure(tibble::tibble(prev = p, ppv, npv),
+            class = c("epi_predval", class(tibble::tibble())))
+}
