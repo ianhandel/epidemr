ebbc27952c7285cfc02e2252ed34ac1c0d93fe03
Ian Handel <ian.handel@ed.ac.uk>
2017-12-24 15:28:45 +0000

    Update demo to shoe epi.tests tidied

--- demo_stuff/epi_tests_demo.R
+++ demo_stuff/epi_tests_demo.R
@@ -6,7 +6,10 @@ table(mtcars$am == 1, mtcars$cyl ==4, dnn = c("am==1", "cyl==4"))
 
 epi_tests(mtcars, am == 1, cyl == 4)
 
-epi_tests(c(8, 5, 3, 16))
-
 epi_tests(table(mtcars$am==1, mtcars$cyl ==4))
 
+res <- epi_tests(c(8, 5, 3, 16))
+
+print(res) # uses epiR::print.epi.tests() method
+
+tidy(res) # uses epidemr::tidy.epi.tests() method
